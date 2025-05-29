9525ff9b97136945cf5601db50a8e0d7aa853cc7
Henrique Paulino <hcpaulino-dev@gmail.com>
2025-05-29 10:34:13 -0300

    Ajustado método DELETE & criado método BUSCAR CLIENTES (Páginação)

--- crm-service/src/main/java/com/example/crm_service/controller/ClienteController.java
+++ crm-service/src/main/java/com/example/crm_service/controller/ClienteController.java
@@ -1,9 +1,14 @@
 package com.example.crm_service.controller;
 
 import com.example.crm_service.dto.CadastrarClienteDto;
+import com.example.crm_service.dto.InformarClienteDto;
 import com.example.crm_service.service.ClienteService;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,11 +33,18 @@ public class ClienteController {
         return ResponseEntity.created(uri).body(cliente);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity deletar(@PathVariable UUID id){
         clienteService.deletar(id);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<InformarClienteDto>> buscarClientes(@PageableDefault(size = 5, sort = "id") Pageable pageable){
+        var clientes = clienteService.buscarClientes(pageable);
+
+        return ResponseEntity.ok(clientes);
 
     }
 }
--- crm-service/src/main/java/com/example/crm_service/service/ClienteService.java
+++ crm-service/src/main/java/com/example/crm_service/service/ClienteService.java
@@ -4,6 +4,8 @@ import com.example.crm_service.dto.CadastrarClienteDto;
 import com.example.crm_service.dto.InformarClienteDto;
 import com.example.crm_service.entity.Cliente;
 import com.example.crm_service.repository.ClienteRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -38,8 +40,14 @@ public class ClienteService {
         clienteRepository.delete(cliente);
     }
 
+    public Page<InformarClienteDto> buscarClientes(Pageable pageable){
+        Page<Cliente> page = clienteRepository.findAll(pageable);
+
+        return page.map(InformarClienteDto::new);
+    }
+
     private Cliente buscarCliente(UUID id){
-        Cliente cliente = clienteRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Cliente não foi encontrado!"))
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não foi encontrado!"));
     }
 }
